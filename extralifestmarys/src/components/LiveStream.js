b74775e9ca2e3c30c1a7e34de3e7878fb1a49622
AimeeJay <ajesso@post.com>
2021-10-22 00:20:40 -0400

    Added css grid boxes for live streams

---
+++ extralifestmarys/src/components/LiveStream.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// reactstrap components
+import {
+  Button,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+} from "reactstrap";
+
+export default function LiveStream() {
+  return (
+    <div>
+<h3 className="d-none d-sm-block"> Live Streams:
+          </h3>
+          <h4>Updated Hourly</h4>
+          <div className="project-container">
+            <a href="https://w4tch-tow3r.herokuapp.com/" className="projects proj1 gradient-box">
+            </a>
+            <div className="projects gradient-box">
+
+            </div>
+            <div className="projects gradient-box">
+
+            </div>
+            <div className="projects gradient-box">
+
+            </div>
+            <div className="projects gradient-box">
+
+            </div>
+            <div className="projects gradient-box">
+
+            </div>
+          </div>
+
+    </div>
+  );
+}
+
+
+
+
+
+
--- extralifestmarys/src/pages/PageHeader.js
+++ extralifestmarys/src/pages/PageHeader.js
@@ -19,9 +19,12 @@ import React from "react";
 import companyLogo from '../assets/img/extralogo.png'
 // reactstrap components
 import { Container } from "reactstrap";
+import LiveStream from "components/LiveStream";
 
 export default function PageHeader() {
   return (
+    <div>
+      <Container>
     <div className="page-header header-filter">
       <div className="squares square1" />
       <div className="squares square2" />
@@ -30,33 +33,18 @@ export default function PageHeader() {
       <div className="squares square5" />
       <div className="squares square6" />
       <div className="squares square7" />
-      <Container>
+      
         <div className="content-center brand">
-        <div className="project-container">
-            <a href="https://w4tch-tow3r.herokuapp.com/" className="projects proj1 gradient-box">
-                </a>
-                <div className="projects proj2 gradient-box">
-                
-                </div>
-                <div className="projects proj3 gradient-box">
-                
-                </div>
-                <div className="projects proj4 gradient-box">
-                
-                </div>
-                <div className="projects proj5 gradient-box">
-                
-                </div>
-                <div className="projects proj6 gradient-box">
-                
-                </div>
-            </div>
-          <h1 className="h1-seo">BLK• React</h1>
-          <img alt="bitcoin" src={companyLogo} />
-          <h3 className="d-none d-sm-block">
-          </h3>
+          <div className="logobox border15">
+            <img className="padding20" alt="bitcoin" src={companyLogo} />
+          </div>
+          <h1 className="">November 6, 2021</h1>
+          <h1 className="">24 hour St. Marys | Game-a-thon</h1>
         </div>
-      </Container>
+      
+    </div>
+    <LiveStream />
+    </Container>
     </div>
   );
 }
