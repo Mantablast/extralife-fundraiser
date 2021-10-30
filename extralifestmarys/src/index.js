2e2cc599059b5f2f7a5bdf101755dbf5ed41d660
AimeeJay <ajesso@post.com>
2021-10-29 20:59:56 -0400

    Reverted to original dom render

--- extralifestmarys/src/index.js
+++ extralifestmarys/src/index.js
@@ -27,11 +27,25 @@ import Index from "views/Index.js";
 import LandingPage from "views/examples/LandingPage.js";
 import RegisterPage from "views/examples/RegisterPage.js";
 import ProfilePage from "views/examples/ProfilePage.js";
-import PageHeader from "pages/PageHeader";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <PageHeader />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" render={(props) => <Index {...props} />} />
+      <Route
+        path="/landing-page"
+        render={(props) => <LandingPage {...props} />}
+      />
+      <Route
+        path="/register-page"
+        render={(props) => <RegisterPage {...props} />}
+      />
+      <Route
+        path="/profile-page"
+        render={(props) => <ProfilePage {...props} />}
+      />
+      <Redirect from="/" to="/" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
